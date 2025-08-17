6c83448be37975817f02070b5fe525ec6a9365c1
CrisKroos25 <cristianguzman0928@gmail.com>
2025-08-16 19:10:03 -0600

    feat(proyect): add Header with styles, logo in public, and basic Card Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+function Header(){
+    return(
+        <>
+        <header className="header">
+            <div className="header__logo-container">
+                <img className="header__logo" src="/pokemon-logo.webp" alt="" />
+            </div>
+            <nav className="header__nav">
+                <ul className="header__nav-list">
+                    <li className="header__nav-item"><a href="#">FESTIVAL DE GO</a></li>
+                    <li className="header__nav-item"><a href="#profile">NOTICIAS</a></li>
+                    <li className="header__nav-item"><a href="#portfolio">TEMPORADAS</a></li>
+                    <li className="header__nav-item"><a href="#form">EVENTOS</a></li>
+                    <li className="header__nav-item"><a href="#">COMUNIDAD</a></li>
+                    <li className="header__nav-item"><a href="#profile">CLASIFICACION</a></li>
+                    <li className="header__nav-item"><a href="#portfolio">TIENDA</a></li>
+                </ul>
+            </nav>
+            <div className="button__container">
+                <button>presiona aqui</button>
+            </div>
+        </header>
+        </>
+    )
+}
+
+export default Header
