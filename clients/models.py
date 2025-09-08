caa3bb274bb956d63d96fd6e116e28943d6d9e17
CrisKroos25 <cristianguzman0928@gmail.com>
2025-09-07 23:16:03 -0600

    feat(proyect): Relating country to client

--- clients/models.py
+++ clients/models.py
@@ -4,6 +4,7 @@ class Clients(models.Model):
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     age = models.IntegerField()
+    country = models.ForeignKey("Country", on_delete=models.SET_NULL, null=True, related_name="clients")
 
     def __str__(self):
         return f"{self.name}"
