29038411d6ada722f273f36122af5724b064c5da
CrisKroos25 <cristianguzman0928@gmail.com>
2025-09-07 22:37:20 -0600

    feat(proyect): Correcting the age attribute to English correctly to avoid mixing languages

--- clients/models.py
+++ clients/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Clients(models.Model):
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
-    edad = models.IntegerField()
+    age = models.IntegerField()
 
     def __str__(self):
         return f"{self.name}"
