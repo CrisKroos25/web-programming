d8323aaa9e6114b84c07472d1b7c651f057ee89f
CrisKroos25 <cristianguzman0928@gmail.com>
2025-09-07 23:03:03 -0600

    feat(proyect): Adding the passive table

--- clients/models.py
+++ clients/models.py
@@ -7,3 +7,7 @@ class Clients(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+    
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=10)
