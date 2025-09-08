286ab51ef4730b593f019e31adfc82fe2021e8a1
CrisKroos25 <cristianguzman0928@gmail.com>
2025-09-07 22:31:07 -0600

    fixed(proyect): Adding the basic initial customer model with first name, last name, and age

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Clients(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    edad = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}"
