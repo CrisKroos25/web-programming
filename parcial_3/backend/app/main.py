2e18569ea8309664321856915fad995d8ca4c826
CrisKroos25 <141697280+CrisKroos25@users.noreply.github.com>
2025-10-12 16:49:19 -0600

    feat(fullstack): Connect React frontend with FastAPI backend and functional Redis (hide and reveal secrets)

--- parcial_3/backend/app/main.py
+++ parcial_3/backend/app/main.py
@@ -1,4 +1,4 @@
-# app/main.py
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from app.api.routes_secrets import router as secrets_router
 
@@ -8,5 +8,14 @@ app = FastAPI(
     description="API para ocultar y revelar mensajes usando Redis",
 )
 
-# Registrar las rutas
-app.include_router(secrets_router, prefix="/api")
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # o ["http://localhost:5173"] si quieres ser más estricto
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#  Registrar las rutas
+app.include_router(secrets_router, prefix="/api", tags=["Secrets"])
